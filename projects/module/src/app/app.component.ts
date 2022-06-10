6c1ec0351bc35de50835e75cb168c8468be80f35
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-06-10 16:24:16 +0200

    parser bugfixes

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import {Component} from '@angular/core';
 import {APP_BASE_HREF} from '@angular/common';
-import {FD_PARTIAL_ORDER, FD_PETRI_NET} from 'ilpn-components';
+import {FD_PARTIAL_ORDER, FD_PETRI_NET, PetriNet, PartialOrder, PetriNetParserService, PartialOrderParserService, DropFile} from 'ilpn-components';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     providers: [
-        // TODO base href
-        {provide: APP_BASE_HREF, useValue: '/ilovepetrinets/'}
+        {provide: APP_BASE_HREF, useValue: '/ilovepetrinets/rabbit/'}
     ]
 })
 export class AppComponent {
@@ -16,4 +15,25 @@ export class AppComponent {
     fdPN = FD_PETRI_NET;
     fdPO = FD_PARTIAL_ORDER
 
+    petriNet: PetriNet | undefined;
+    partialOrder: PartialOrder | undefined;
+
+    constructor(private _pnParser: PetriNetParserService, private _poParser: PartialOrderParserService) {
+    }
+
+    parsePN(files: Array<DropFile>) {
+        if (files.length === 0) {
+            this.petriNet = undefined;
+        }
+        this.petriNet = this._pnParser.parse(files[0].content);
+        console.dir(this.petriNet);
+    }
+
+    parsePO(files: Array<DropFile>) {
+        if (files.length === 0) {
+            this.partialOrder = undefined;
+        }
+        this.partialOrder = this._poParser.parse(files[0].content);
+        console.dir(this.partialOrder);
+    }
 }
